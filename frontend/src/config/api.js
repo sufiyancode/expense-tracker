4c56f45ccd5efd066f2fd41531825d459eb1020b
Shaikh Sufiyan <sksufiyan9860@gmail.com>
2024-12-24 20:31:02 +0530

    add backend url

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,6 +1,7 @@
 import React from "react";
 
-export const API_BASE_URL = "http://127.0.0.1:5000/api/v1";
+export const API_BASE_URL =
+  "https://expense-tracker-mu-sage-83.vercel.app/api/v1";
 
 export const ENDPOINTS = {
   LOGIN: `${API_BASE_URL}/users/login`,
